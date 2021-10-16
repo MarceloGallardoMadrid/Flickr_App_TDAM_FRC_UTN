5aff2315157ae6d36651c2e1716e43d444c481fc
MarceloGallardoMadrid <nahuel.n.piguillem@gmail.com>
2021-10-16 16:45:39 -0300

    Probando el commit

---
+++ app/src/main/java/com/nahue/flickrapp/About.java
@@ -0,0 +1,16 @@
+package com.nahue.flickrapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class About extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        Toast.makeText(getApplicationContext(),"About us",Toast.LENGTH_SHORT);
+    }
+}
