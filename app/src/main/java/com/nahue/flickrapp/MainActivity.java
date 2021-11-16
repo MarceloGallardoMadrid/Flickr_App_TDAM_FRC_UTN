7539f9e9850f292cdf7ba09226c87c33301b53db
riverospm <riverospm@hotmail.com>
2021-11-15 22:28:00 -0300

    SE arreglo la muestra del recycler del main.

--- app/src/main/java/com/nahue/flickrapp/MainActivity.java
+++ app/src/main/java/com/nahue/flickrapp/MainActivity.java
@@ -28,17 +28,11 @@ public class MainActivity extends AppCompatActivity {
         MyApplication.INTERNET_CONEX=verifyConection();
         notifyLackOfInternet(MyApplication.INTERNET_CONEX);
         listadetalle=new ArrayList<>();
-        cargarDetalle();
+
         RecyclerAlbumFragment frag = new RecyclerAlbumFragment();
         FragmentTransaction transaction= getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.frame_container,frag);
         transaction.commit();
-        findViewById(R.id.btnRoom).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(),TestRoom.class));
-            }
-        });
 
     }
 
@@ -50,20 +44,6 @@ public class MainActivity extends AppCompatActivity {
 
     private int verifyConection() {
         return NotificationUtil.verifyConection(this);
-
-    }
-
-
-    private void cargarDetalle() {
-        listadetalle.add(new EntidadDetalle("Nueva Foto",R.drawable.foto1));
-        listadetalle.add(new EntidadDetalle("Disfrutando el paisaje",R.drawable.foto2));
-        listadetalle.add(new EntidadDetalle("Disfrutando la vida",R.drawable.foto3));
-        listadetalle.add(new EntidadDetalle("Haciendo ejemplos",R.drawable.foto4));
-        listadetalle.add(new EntidadDetalle("Metiéndole a full para que salga",R.drawable.foto5));
-        listadetalle.add(new EntidadDetalle("Ver que pasa si es una descripción muy larga que no entra",R.drawable.foto6));
-        listadetalle.add(new EntidadDetalle("Bien",R.drawable.foto7));
-        listadetalle.add(new EntidadDetalle("Excelente",R.drawable.foto8));
-
 
     }
 
